fb84688aa2f9ee2e44d9b0b19f776282830b472f
daseulsong <vates0328@gmail.com>
2020-12-11 23:09:37 +0900

    Refactor the action creator with an arrow function form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,7 @@
 import jsonplaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-  // Bad approach! because it breaks the rules of redux(action creator)
+export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get("/posts");
 
-  return {
-    type: "FETCH_POSTS",
-    payload: response,
-  };
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
-
-//this bad approach causes an error saying
-// 1. Actions must be plain objects.
-// 2. Use custom middleware for async actions. => this will be solved by making use of redux-thunk.
