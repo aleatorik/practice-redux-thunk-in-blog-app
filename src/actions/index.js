29a446193a64e8f9b81db56aa5e7456f4dc683e0
daseulsong <vates0328@gmail.com>
2020-12-11 21:47:29 +0900

    Add jsonPlaceholder API to the action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
-export const fetchPosts = () => {
+import jsonplaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async () => {
+  // Bad approach! because it breaks the rules of redux(action creator)
+  const response = await jsonplaceholder.get("/posts");
+
   return {
     type: "FETCH_POSTS",
+    payload: response,
   };
 };
+
+//this bad approach causes an error saying
+// 1. Actions must be plain objects.
+// 2. Use custom middleware for async actions. => this will be solved by making use of redux-thunk.
