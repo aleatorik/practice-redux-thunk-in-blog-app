4f538dff1b27b2d78f658d9b03fcf3fe3ff6be0b
daseulsong <vates0328@gmail.com>
2020-12-13 17:51:29 +0900

    chore: Correct a typo

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -19,7 +19,7 @@ class UserHeader extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return { users: state.users.find((user) => user.id === ownProps.userId) };
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUsers })(UserHeader);
