69d09bff41d52ee30dc8d45799af67fa40f5146a
daseulsong <vates0328@gmail.com>
2020-12-13 16:34:22 +0900

    Refactor adopting memoization to fetch users only one time for each

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonplaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get("/posts");
@@ -6,8 +7,10 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUsers = (id) => async (dispatch) => {
+export const fetchUsers = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  // ' _ ' <- means private function so that other devs should not attempt to call this
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
